8ea9a84278079f9762f9a424564b6112fbde222f
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-09-26 14:28:27 +0200

    local words dataset

--- src/server/routers/word.ts
+++ src/server/routers/word.ts
@@ -1,17 +1,20 @@
-import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 
+const words = fs.readFileSync(path.join(__dirname, '..', '..', '..', '..', '..', 'words.txt')).toString().split('\r\n');
+
 const wordRouter = router({
-    randomWord: publicProcedure.query(async () => {
-        if (process.env.NODE_ENV !== 'production') return { word: 'development' };
-        const { data } = await axios.get('https://api.api-ninjas.com/v1/randomword', {
-            headers: {
-                'X-Api-Key': process.env.API_KEY,
-            },
-            params: {},
-        });
-        return { word: (data as { word: string[] }).word[0] };
-    }),
+    randomWord: publicProcedure
+        .input(z.object({ limit: z.number().min(1).max(50).default(50) }))
+        .query(async ({ input }) => {
+            const res = [];
+            for (let i = 0; i < input.limit; i++) {
+                res.push(words[Math.floor(Math.random() * words.length)]);
+            }
+            return { words: res };
+        }),
 });
 
 export default wordRouter;
